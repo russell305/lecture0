358a3576b132e86ca33b4586f0be5cab6082b70e
Russell Mckee <russm305@gmail.com>
2018-12-24 09:38:03 -0500

    more

--- application.py
+++ application.py
@@ -1,11 +1,19 @@
-from flask import Flask # Import the class `Flask` from the `flask` module, written by someone else.
+from flask import Flask, render_template # Import the class `Flask` from the `flask` module, written by someone else.
+#export FLASK_APP=application.py
 
 app = Flask(__name__) # Instantiate a new web application called `app`, with `__name__` representing the current file
 
 @app.route("/") # A decorator; when the user goes to the route `/`, exceute the function immediately below
 def index():
-	return render_template("index.html")
+	#return "Hello World"
+	headline = "Hello Russ"
+	return render_template("index.html", headline=headline)
 
-@app.route("/david")
-def david():
-	return "Hello David"
+#@app.route("/david")
+#def david():
+	#return "Hello David"
+
+#@app.route("/<string:name>")
+#def hello(name):
+	#name=name.capitalize()
+	#return f"Hello, {name}!"
