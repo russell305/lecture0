9444addf6d369644d8ce661e915895db30efd1aa
Russell Mckee <russm305@gmail.com>
2018-12-26 11:37:56 -0500

    flask loops

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask, render_template # Import the class `Flask` from the `flask` module, written by someone else.
 #export FLASK_APP=application.py
 
@@ -5,9 +6,28 @@ app = Flask(__name__) # Instantiate a new web application called `app`, with `__
 
 @app.route("/") # A decorator; when the user goes to the route `/`, exceute the function immediately below
 def index():
+	#conditional
+	#now = datetime.datetime.now()
+	#new_year = now.month == 12 and now.day == 25
+	#return render_template("index.html", new_year=new_year)
+
+	#for looop
+	#names = ["Alice", "Bob", "Cahlelie"]
+	#return render_template("index.html", names=names)
+
 	#return "Hello World"
 	headline = "Hello Russ"
 	return render_template("index.html", headline=headline)
+
+@app.route("/more")
+def more():
+	return render_template("about.html")	
+
+@app.route("/bye")
+def bye():
+	headline = "goodbye"
+	return render_template("about.html")	
+
 
 #@app.route("/david")
 #def david():
