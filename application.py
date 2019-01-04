6978848594cfbdc0f202b22cb48c5352cebc4ac9
Russell Mckee <russm305@gmail.com>
2019-01-04 15:16:02 -0500

    flask sessions

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session (app)
 
-note_pad = []
+#note_pad = []
 
 
 @app.route("/", methods = ["GET", "POST"]) # A decorator; when the user goes to the route `/`, exceute the function immediately below
@@ -18,24 +18,29 @@ def index():
 	#conditional
 	#now = datetime.datetime.now()
 	#new_year = now.month == 12 and now.day == 25
+	new_year = True
 	#return render_template("index.html", new_year=new_year)
 
 	#for looop
-	#names = ["Alice", "Bob", "Cahlelie"]
+	names = ["Alice", "Bob", "Cahlelie"]
 	#return render_template("index.html", names=names)
 
 	#return "Hello World"
 	headline = "Hello Russ"
-	return render_template("index.html", headline=headline)
+	return render_template("index.html", headline=headline, names=names, new_year=new_year)
 
 @app.route("/notes", methods = ["GET", "POST"])
-def notes():	
+def notes():
+	if session.get("note_pad") is None:
+			session["note_pad"] = []	
+
 	if request.method=="POST":
 		note = 	request.form.get("note")
 		print (note)
-
-		note_pad.append(note)
-		return render_template("notes.html", note_pad=note_pad) 
+		session["note_pad"].append(note)
+		print (session["note_pad"])
+		#note_pad.append(note)
+		return render_template("notes.html", note_pad=session["note_pad"]) 
 	else: 	return render_template("notes.html")
 
 @app.route("/hello", methods = ["POST"])
@@ -57,11 +62,6 @@ def bye():
 
 @app.route("/inherit", methods = ["GET", "POST"])
 def inherit():
-	# if request.method=="GET":
-		# note = 	request.form.get("note")
-		# notes.append(note)
-
-		#return render_template("notes.html")
 	return render_template("inherit.html")
 
 @app.route("/inherit1")
@@ -77,3 +77,5 @@ def inherit1():
 #def hello(name):
 	#name=name.capitalize()
 	#return f"Hello, {name}!"
+
+
