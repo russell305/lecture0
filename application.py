323c584a62b1eed15a04ccadb21a0904629f5d13
Russell Mckee <russm305@gmail.com>
2019-02-17 15:08:23 -0500

    UI/UX

--- application.py
+++ application.py
@@ -5,36 +5,19 @@
 #import datetime
 from flask import Flask, render_template, request, session, jsonify# Import the class `Flask` from the `flask` module, written by someone else.
 from flask_session import Session
-
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from pprint import pprint
 #import csv
+import json #for Python to Javascript
 import requests #for JSON
 from flights import Flight
 from mechanic import Mechanic
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-
-import json
-
-
-
-
-
 app = Flask(__name__) # Instantiate a new web application called `app`, with `__name__` representing the current file
 
 GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
 
-params = {
-	'address': '665 ne 83 terrace, miami fl',
-    'key': 'AIzaSyD9fytSdXXr6kVZdXLddFJyF9HT4JTt-qM',
-}
-
-        # Do the request and get the response data
-response = requests.get(GOOGLE_MAPS_API_URL, params=params)
-#response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyD9fytSdXXr6kVZdXLddFJyF9HT4JTt-qM')
-resp_json_payload = response.json()
-#print(resp_json_payload)
-print(resp_json_payload['results'][0]['geometry']['location'])
 #app.config["SESSION_PERMANENT"] = False
 #app.config["SESSION_TYPE"] = "filesystem"
 #Session (app)
@@ -48,6 +31,7 @@ db = scoped_session(sessionmaker(bind=engine)) # for individual sessions
 #print("russ_api", russ_api)
 flight_list=[]
 mechanic_list=[]
+
 f1 = Flight(origin="New York", destination="Paris", duration=540)
 flight_list.append(f1)
 f2 = Flight(origin="la", destination="tokyo", duration=640)
@@ -58,7 +42,7 @@ flight_list.append(f3)
 #mechanic = Mechanic("russ", "johnson", "12344", "russm305@fmail.com")
 #mechanic_list.append(mechanic)
 
-print("flight", flight_list[2].destination)
+#print("flight", flight_list[2].destination)
 #res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "0S6vFxQgJiRIw2CZbc2Yg", "isbns": "9781632168146"})
 #print(res.json())
 #result_1 = res.json()
@@ -69,26 +53,41 @@ print("flight", flight_list[2].destination)
 #db.execute("CREATE TABLE flights(id SERIAL PRIMARY KEY, origin VARCHAR NOT NULL, destination VARCHAR NOT NULL, duration INTEGER NOT NULL)")
 #db.execute("CREATE TABLE books2(id SERIAL PRIMARY KEY, isbn VARCHAR NOT NULL, title VARCHAR NOT NULL, author VARCHAR NOT NULL, year INTEGER NOT NULL)")
 
-
 	#db.execute("DELETE FROM books1 WHERE id >'29'")
 #for i in range(20):
 #db.execute("INSERT INTO flights (origin, destination) VALUES ('New Delhi', 'Miami')")
 #db.execute("INSERT INTO flights (origin, destination) VALUES ('Atlanta', 'Bogota', '655')")
 #db.execute("INSERT INTO flights (origin, destination, duration) VALUES ('NY', 'Tokyo', '424')")
+#sample injection code defense... db.execute("INSERT INTO passengers (name, flight_id) VALUES (:name, :flight_id)", {"name": name, "flight_id": flight_id})
+
 #db.commit()
 
 
-#flights = db.execute("SELECT * FROM flights3").fetchall()
+flights = db.execute("SELECT * FROM flights3").fetchall()
 #books1 = db.execute("SELECT * FROM books_2").fetchall()
 #top3= db.execute("SELECT * FROM books_2 ORDER BY id ASC LIMIT 3").fetchall()
 #for flight in flights:
 	#print(f"{flight.origin} to {flight.destination} minutes.")
 
-#print("flights",flights)
+	#wont need this, set up database to take data from form and geocode and update datbase
+for i in flights:
+	params = {
+		'address': i.origin,
+	    'key': 'AIzaSyD9fytSdXXr6kVZdXLddFJyF9HT4JTt-qM',
+	}
+	#res = requests.get(GOOGLE_MAPS_API_URL, params=params)
+#respnse = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyD9fytSdXXr6kVZdXLddFJyF9HT4JTt-qM')
+	#response = res.json()
+#print("jsonresonse",response)
+	#print("jsonresonse1",response['results'][0]['geometry']['location'])
+
+print("flights",flights)
+print("flights",flights[0])
+print("flights",flights[1].origin)
 #print("books1",books1)
 #print("top3", top3)
-
-
+flight_origin =[]
+len(flight_origin)
 @app.route("/", methods = ["GET"]) # A decorator; when the user goes to the route `/`, exceute the function immediately below
 def index():
 
@@ -105,44 +104,45 @@ def inherit1():
 	address = request.form.get("address")
 	description = request.form.get("description")
 
-	mechanic_name={'mechanic_name': name}
-	mechanic_email={'mechanic_email': email}
-	mechanic_phone={'mechanic_phone': phone}
-	mechanic_address={'mechanic_address': address}
-	mechanic_description={'mechanic_address': description}
-	g = geocoder.google('453 Booth Street, Ottawa ON')
-	latlng = g.latlng
-	print("MV", g)
+	mechanic = Mechanic( name, email, phone, address, description)
+	#database create and read
+	mechanic_list.append(mechanic)
+	for i in mechanic_list:
+		try:
+		  mech_num
+		except NameError:
+		  mech_num=0
+		  print ("well, it WASN'T defined after all!")
+		else:
+		  print ("sure, it was defined.")
+		  #mech_num = mech_num + 1
 
+		mechanic_name={'mechanic_name': i.name}
+		mechanic_email={'mechanic_email': i.email}
+		mechanic_phone={'mechanic_phone': i.phone}
+		mechanic_address={'mechanic_address': i.address}
+		mechanic_description={'mechanic_address': i.description}
+		pprint(vars(mechanic_list[mech_num]))
+
+
+	for i in flights:
+		print ("ruusman",i.origin)
+		origin = {'origin': i.origin}
+		flight_origin.append(origin)
+
+	print("flight_origin", flight_origin)
+	print("flight_origin", flight_origin[1])
+	List_length = len(flight_origin)
+	print("List_length",List_length)
+
+
+
+#//I can use mechanic_name instaed of mechanic_id
 	mechanic_id = 0
 	mechanic_idj={'mechanic_idj': mechanic_id}
 
-	datar= {}
-
-	datar ["gravity"] = {
-		"mediator":"gravitons",
-		"relative strength" : "1",
-		"range" : "infinity"
-		}
-	datar ["weak"] = {
-		"mediator":"W/Z bosons",
-		"relative strength" : "10^25",
-		"range" : "10^-18"
-		}
-
-	print(mechanic_id)
-
-
-
-
 	print("mechanic_name", mechanic_name["mechanic_name"])
-
-
-	mechanic = Mechanic( mechanic_name, mechanic_email, mechanic_phone, mechanic_address, description)
-
-	mechanic_list.append(mechanic)
-
-	return render_template("inherit1.html", mechanic_list=mechanic_list, mechanic_address=mechanic_address, datar=datar, mechanic_id=mechanic_id, mechanic_idj=mechanic_idj )
+	return render_template("inherit1.html",flight_origin=flight_origin,  mechanic_name=mechanic_name, mechanic_phone=mechanic_phone, mechanic_list=mechanic_list, mechanic_address=mechanic_address,  mechanic_id=mechanic_id, mechanic_idj=mechanic_idj )
 
 @app.route("/mechanic/<string:full_name>")
 def mechanic(full_name):
